2ae079b7f309a9a19968fb1f721f08c832bb89c0
andaraleiliapriza-cloud <andaraleiliapriza@gmail.com>
2026-08-04 13:12:43 +0700

    tambahkan kategori

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Gagal konek ke database:', err);
+  } else {
+    console.log('Berhasil konek ke database Glowlist');
+  }
+});
 
 app.get('/', (req, res) => {
     res.send('Selamat Datangg di GlowList APIIIII-!!!!');
+});
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(result);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(result);
+    });
 });
 
 app.listen(PORT, () => {
